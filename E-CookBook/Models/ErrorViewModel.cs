9a3560ba350b7c77350f92705ef2921fb3b79547
Lizzie219 <cs.lizett9@stud.uni-obuda.hu>
2023-08-27 20:55:07 +0200

    Add project files.

---
+++ E-CookBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_CookBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
