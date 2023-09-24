cad29645c2345ce6c45fc21dd6e2f8f989d50fff
Lizzie219 <cs.lizett9@stud.uni-obuda.hu>
2023-09-24 21:34:07 +0200

    Recipe controller and view created

---
+++ E-CookBook/Controllers/RecipesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using E_CookBook.Data;
+using E_CookBook.Models;
+
+namespace E_CookBook.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly TastyDbContext _context;
+
+        public RecipesController(TastyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Recipes
+        public async Task<IActionResult> Index()
+        {
+            var tastyDbContext = _context.Recipe.Include(r => r.Category).Include(r => r.PriceCategory);
+            return View(await tastyDbContext.ToListAsync());
+        }
+
+        // GET: Recipes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Recipe == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipe
+                .Include(r => r.Category)
+                .Include(r => r.PriceCategory)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        // GET: Recipes/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryID"] = new SelectList(_context.Category, "ID", "ID");
+            ViewData["PriceCategoryID"] = new SelectList(_context.PriceCategory, "ID", "ID");
+            return View();
+        }
+
+        // POST: Recipes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,PhotoLocation,CookingTime,Portion,Instructions,Source,CategoryID,PriceCategoryID")] Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recipe);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryID"] = new SelectList(_context.Category, "ID", "ID", recipe.CategoryID);
+            ViewData["PriceCategoryID"] = new SelectList(_context.PriceCategory, "ID", "ID", recipe.PriceCategoryID);
+            return View(recipe);
+        }
+
+        // GET: Recipes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Recipe == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipe.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryID"] = new SelectList(_context.Category, "ID", "ID", recipe.CategoryID);
+            ViewData["PriceCategoryID"] = new SelectList(_context.PriceCategory, "ID", "ID", recipe.PriceCategoryID);
+            return View(recipe);
+        }
+
+        // POST: Recipes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,PhotoLocation,CookingTime,Portion,Instructions,Source,CategoryID,PriceCategoryID")] Recipe recipe)
+        {
+            if (id != recipe.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recipe);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecipeExists(recipe.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryID"] = new SelectList(_context.Category, "ID", "ID", recipe.CategoryID);
+            ViewData["PriceCategoryID"] = new SelectList(_context.PriceCategory, "ID", "ID", recipe.PriceCategoryID);
+            return View(recipe);
+        }
+
+        // GET: Recipes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Recipe == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipe
+                .Include(r => r.Category)
+                .Include(r => r.PriceCategory)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        // POST: Recipes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Recipe == null)
+            {
+                return Problem("Entity set 'TastyDbContext.Recipe'  is null.");
+            }
+            var recipe = await _context.Recipe.FindAsync(id);
+            if (recipe != null)
+            {
+                _context.Recipe.Remove(recipe);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecipeExists(int id)
+        {
+          return (_context.Recipe?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
